d58725dde3cde5bd9ff7eba8df8eaeb86315a51c
Thang cg <duongsag.hp11@gmail.com>
2022-07-04 15:11:52 +0700

    yo

---
+++ main.js
@@ -0,0 +1,5 @@
+let arr = ["sunday", "monday", "tuesday", "wednesday", "thursday","friday", "saturday"];
+
+const date = new Date()
+const day = date.getDay()
+console.log(`hom nay la thu ${arr[day]}`)
